cecae3c699a1141d0f0b5a325693425cdd33afcb
hrycykj <john@quillconsulting.ca>
2022-01-18 14:08:12 -0700

    created custom marker component

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+
+import Marker from './components/Marker'
+import markerMe from './assets/map_pin2.png'
 
 MapboxGL.setAccessToken('pk.eyJ1Ijoid29vamFlIiwiYSI6ImNreWR5b3UxNTBjMGoyb3NoMjk4eXUxbHcifQ.n4z9_obSqP5bOL6Sq-T5tA');
 let coordsSaddledome=[-114.0519, 51.0374]
 let coordsMe=[-114.20497, 51.071891317967605]
 
 export default function App() {
+
   return (
     <View style={styles.page}>
       <Text>Open up App.js to start working on your new app!</Text>
@@ -25,24 +29,13 @@ export default function App() {
               animationMode={'flyTo'}
               animationDuration={0}
           	/>
-            <MapboxGL.Camera
-              zoomLevel={15}
-              // bearing={0}
-              // pitch={0}
-              centerCoordinate={coordsSaddledome}
-              animationMode={'flyTo'}
-              animationDuration={5000}
-            />
-            <MapboxGL.PointAnnotation 
-              id={'1'}
-              title= {'Checkpoint #1'}
-              draggable={true}
-              coordinate={coordsSaddledome}
+            <Marker
+              coords = {coordsMe}
+              img = {markerMe}
             />
             {/* <MapboxGL.UserLocation
               visible = {true}
             /> */}
-
           </MapboxGL.MapView>
         </View>
       <StatusBar style="auto" />
---
+++ client-mobile/components/Marker.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MapboxGL from '@react-native-mapbox-gl/maps';
+
+const Marker = (props) => {
+    let coords = props.coords
+    let img = props.img
+    
+    return (
+        <MapboxGL.PointAnnotation 
+                id={'1'}
+                title= {'Checkpoint #1'}
+                draggable={true}
+                coordinate={coords}
+        >
+            <Image source={img}
+                style={{
+                height: 35, width: 35,
+                // tintColor: '#fff'
+                }}
+            />
+        </MapboxGL.PointAnnotation>
+
+    )
+}
+
+export default Marker
